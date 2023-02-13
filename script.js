09498a1582f3b2e6a974bb941e49eea15d7ca8bf
mgkbrela <98755684+mgkbrela@users.noreply.github.com>
2023-02-14 01:33:18 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+window.onload = function(){
+    var img = document.getElementById("popcat1"); 
+    var audio = new Audio("pop.mp3");
+
+    img.addEventListener('mousedown', function (){
+        img.src = '2.png';
+        audio.play();
+    });
+
+    img.addEventListener('mouseup', function () {
+        img.src = '1.png';
+    });
+   
+
+}
