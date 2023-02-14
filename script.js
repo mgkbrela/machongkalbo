4955b04746f83e56eafede403cf47f491ea8f478
mgkbrela <98755684+mgkbrela@users.noreply.github.com>
2023-02-14 10:22:17 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ window.onload = function(){
     var img = document.getElementById("popcat1"); 
     var audio = new Audio("pop.mp3");
 
-    img.addEventListener('click', function (){
+    img.addEventListener('mouseup', function (){
         img.src = '2.png';
         audio.play();
     });
 
-    img.addEventListener('click', function (){
+    img.addEventListener('mousedown', function (){
         img.src = '3.png';
         audio.play();
     });
